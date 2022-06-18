751c400ec57578d3bea3c0ca5e3627ba0783a3ba
Bohdan Onyshko <96291948+Bohdan171@users.noreply.github.com>
2022-06-18 20:33:36 +0300

    1 задание Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+stopBtnEl.disabled = true;
+let intervalID = null;
 
+const randomBodyColorGenerator = {
+  DELAY: 1000,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    intervalID = setInterval(() => {
+      changeBgColorRandom();
+    }, this.DELAY);
+    
+  },
+
+  start() {
+    startBtnEl.addEventListener('click', () => {
+      this.interval();
+      startBtnEl.disabled = true;
+      stopBtnEl.disabled = false;
+    });
+    stopBtnEl.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(intervalID);
+    stopBtnEl.disabled = true;
+    startBtnEl.disabled = false;
+  },
+};
+
+function changeBgColorRandom() {
+  bodyEl.style.backgroundColor = `${randomBodyColorGenerator.getRandomHexColor()}`;
+}
+
+randomBodyColorGenerator.start();
